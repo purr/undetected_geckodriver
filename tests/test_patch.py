4f1bdb2c85fd84823ae44b95f3c123026b614ec9
Olivia <oliviawolfie@pm.me>
2025-05-22 02:28:22 +0200

    Add additional test-case that checks navigator.webdriver in the browser directly

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -4,24 +4,28 @@ import undetected_geckodriver
 
 
 class TestPatch(unittest.TestCase):
+
+    def setUp(self):
+        overridden_path = os.environ.get("ACTIONS_FF_OVERRIDE")
+        if (overridden_path is not None):
+            overridden_path = os.path.abspath(overridden_path)
+
+        self.driver = undetected_geckodriver.Firefox(
+            lookup_path=overridden_path
+        )
+    def tearDown(self):
+        self.driver.quit()
+
     def test_patch(self):
         """
         Tests whether or not the patch took hold at the expected location.
         This will fail on unsupported operating systems, or operating systems
         where the patch just generally fails for whatever reason.
         """
-        overridden_path = os.environ.get("ACTIONS_FF_OVERRIDE")
-        if (overridden_path is not None):
-            overridden_path = os.path.abspath(overridden_path)
-
-        driver = undetected_geckodriver.Firefox(
-            lookup_path=overridden_path
-        )
-        driver.quit()
-        dir = driver._get_undetected_firefox_path()
+        dir = self.driver._get_undetected_firefox_path()
         patched_file = os.path.join(
             dir,
-            driver._platform_dependent_params["xul"]
+            self.driver._platform_dependent_params["xul"]
         )
 
         with open(patched_file, "rb") as file:
@@ -30,6 +34,14 @@ class TestPatch(unittest.TestCase):
         self.assertTrue(len(libxul_data) > 0)
         self.assertFalse(
             b"webdriver" in libxul_data
+        )
+
+    def test_webdriver_gone(self):
+        self.assertTrue(
+            self.driver.execute_script("return navigator !== null")
+        )
+        self.assertIsNone(
+            self.driver.execute_script("return navigator.webdriver")
         )
 
 if __name__ == '__main__':
